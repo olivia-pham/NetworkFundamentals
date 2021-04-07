2c2fee7ee0332cb22a70c1a396517e2d8ddd2c05
olivia-pham <62281454+olivia-pham@users.noreply.github.com>
2021-04-07 15:15:23 +1000

    Update server.py

--- NetFun WebServer/server.py
+++ NetFun WebServer/server.py
@@ -1,10 +1,13 @@
+#import socket module
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
+#Prepare a server socket
 serverPort = 80
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print("Webserver is on port: ", serverPort)
 while True:
+    #Establish the connection
     print("Ready to serve")
     connectionSocket, addr = serverSocket.accept()
     try:
@@ -12,6 +15,7 @@ while True:
         filename = message.split()[1]
         f = open(filename[1:])
         outputdata = f.read()
+        #Send the content of the required file to the client
         connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -19,9 +23,11 @@ while True:
         connectionSocket.close()
         print("Message Successfully Sent")
     except IOError:
+        #Send response message for file not found
         connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
         connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
         print("404 Error not Found")
+        #Close client socket
         connectionSocket.close()
 serverSocket.close()
 sys.exit()
