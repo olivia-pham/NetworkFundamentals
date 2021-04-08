4e0769faca3247297548bc6115dd875165e6ebe0
olivia-pham <62281454+olivia-pham@users.noreply.github.com>
2021-04-08 18:34:16 +1000

    Update server.py

--- NetFun WebServer/server.py
+++ NetFun WebServer/server.py
@@ -1,34 +1,36 @@
-#import socket module
+#import python socket library
 from socket import *
+#import sys module
 import sys
-serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = 80
-serverSocket.bind(('', serverPort))
+serverSocket.bind(('', serverPort)) #bind socket to port 80
 serverSocket.listen(1)
 print("Webserver is on port: ", serverPort)
-while True:
+
+while True: #loop
     #Establish the connection
     print("Ready to serve")
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept() #server waits on accept() for incoming requests, new socket created on return
     try:
-        message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:])
+        message = connectionSocket.recv(1024) #
+        filename = message.split()[1] #split string message into list, return element at index 1
+        f = open(filename[1:]) #
         outputdata = f.read()
         #Send the content of the required file to the client
-        connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n") 
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
         print("Message Successfully Sent")
-    except IOError:
+    except IOError: 
         #Send response message for file not found
         connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
+        connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n") #print in html page
         print("404 Error not Found")
         #Close client socket
         connectionSocket.close()
     serverSocket.close()
-    sys.exit()
+    sys.exit() #exit
